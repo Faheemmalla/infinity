d28edec43ca422140c2c7308c62aa4782c8c4e5b
Faheemmalla <faheem.yousuf2004@gmail.com>
2025-09-17 19:17:09 +0530

    Initial Commit

---
+++ infinity/infinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  infinityApp.swift
+//  infinity
+//
+//  Created by faheem yousuf malla on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct infinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
